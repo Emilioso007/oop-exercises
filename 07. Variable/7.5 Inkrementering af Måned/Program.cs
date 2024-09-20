b177bdd27e93177e74794e2b85a60574fb0ceb56
Emil Jørgensen <emiljorgensen05@gmail.com>
2024-09-20 22:53:31 +0200

    7.5 - 7.9 done!

---
+++ 07. Variable/7.5 Inkrementering af Måned/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Program
+{
+	public static void Main(string[] args)
+	{
+		Console.Clear();
+
+		double month = 2;
+
+		Console.WriteLine(month);
+
+		month += 0.5;
+
+		Console.WriteLine(month);
+
+		month += 0.5;
+
+		Console.WriteLine(month);
+
+		Console.ReadKey();
+	}
+}
