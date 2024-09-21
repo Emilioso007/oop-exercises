04827f108e10765f4eeb5893fa8ab7ba43e7cc04
Emil Jørgensen <emiljorgensen05@gmail.com>
2024-09-21 20:02:01 +0200

    12 + 13 done

---
+++ 13. Enum/13.1 Retning/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Program
+{
+	public static void Main(string[] args)
+	{
+		Console.Clear();
+
+		Direction heading = Direction.North;
+
+		Console.WriteLine(heading);
+
+		Console.ReadKey();
+	}
+
+	public enum Direction {
+		North,
+		South,
+		East,
+		West,
+	}
+}
