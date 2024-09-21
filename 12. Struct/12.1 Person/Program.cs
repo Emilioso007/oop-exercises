764c50c4f7a4373acbe25307621a92fa4bb0534b
Emil Jørgensen <emiljorgensen05@gmail.com>
2024-09-21 19:49:19 +0200

    12. Done

---
+++ 12. Struct/12.1 Person/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Program
+{
+
+	public struct Person {
+		public string name;
+	}
+
+	public static void Main(string[] args)
+	{
+		Console.Clear();
+
+		Person teacher = new Person();
+		teacher.name = "Aslak";
+
+		Console.WriteLine(teacher.name);
+
+		Console.ReadKey();
+	}
+}
