5f3d727c82c139144adc4a3c52e71307f6c7468a
Emil Jørgensen <emiljorgensen05@gmail.com>
2024-09-23 15:55:25 +0200

    15.3 done

---
+++ 15. Exceptions/15.3 Gennemsnitlig Karakter/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class AvgGradeError
+{
+
+	public static int[] grades = {4, 7, 02, 00, 10, 4, 12};
+
+	public static void Main(string[] args)
+	{
+		Console.Clear();
+
+		int count = 0;
+		int sum = 0;
+
+		for (int i = 0; i < grades.Length; i++) {
+			try {
+				sum += GetGrade(i);
+				count++;
+			} catch (Exception e) {
+				Console.WriteLine($"courseId {i}: {e.Message}");
+			}
+		}
+
+		Console.WriteLine($"Average: {(double)sum/count}");
+
+		Console.ReadKey();
+	}
+
+	public static int GetGrade(int courseId) {
+		int grade = grades[courseId];
+
+		if (grade < 02) {
+			throw new Exception("Failed!");
+		}
+
+		return grade;
+	}
+}
