d483d0d12221c55c07b670a296c2e279998b6911
Emil Jørgensen <emiljorgensen05@gmail.com>
2024-09-23 15:45:17 +0200

    15.2 done

---
+++ 15. Exceptions/15.2 Kontoer/2. while loop catch/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Program
+{
+
+	public static int[] accounts = { 903, 716, 67};
+
+	public static void Main(string[] args)
+	{
+		Console.Clear();
+
+		while (true) {
+			int accountId = GetAccountNumber();
+			try {
+				PrintAccountState(accountId);
+			} catch {
+
+			}
+		}
+
+		Console.ReadKey();
+	}
+
+	public static int GetAccountNumber() {
+		Console.WriteLine("Enter an account number: ");
+		return Convert.ToInt32(Console.ReadLine());
+	}
+
+	public static void PrintAccountState(int accountId) {
+		Console.WriteLine($"Account {accountId} contains {accounts[accountId]}");
+	}
+}
---
+++ 15. Exceptions/15.2 Kontoer/4. convert catch/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public class Program
+{
+
+	public static int[] accounts = { 903, 716, 67};
+
+	public static void Main(string[] args)
+	{
+		Console.Clear();
+
+		while (true) {
+			int accountId = GetAccountNumber();
+			try {
+				PrintAccountState(accountId);
+			} catch {
+
+			}
+		}
+
+		Console.ReadKey();
+	}
+
+	public static int GetAccountNumber() {
+		Console.WriteLine("Enter an account number: ");
+		try {
+			return Convert.ToInt32(Console.ReadLine());
+		} catch {
+			return -1;
+		}
+	}
+
+	public static void PrintAccountState(int accountId) {
+		Console.WriteLine($"Account {accountId} contains {accounts[accountId]}");
+	}
+}
