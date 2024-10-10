cc00bbb83f58022ed9c775d8846bf219e358d7e5
Emil Jørgensen <emiljorgensen05@gmail.com>
2024-10-10 22:03:49 +0200

    21.3+21.4 done

---
+++ 21. Navngivning/21.4 Scope/Program.cs
@@ -0,0 +1,22 @@
+﻿class Scope {
+
+	static int bonus = 42;
+
+	static int Doubler (int value)
+	{
+		int d = value * 2;
+		return d;
+	}
+
+	public static void Main (string[] args)
+	{
+		int sum = 0;
+		for (int i = 0; i < 100 ; i++)
+		{
+			int tmp = Doubler(i);
+			sum += tmp;
+		}
+
+		Console.WriteLine(sum + bonus);
+	}
+}
