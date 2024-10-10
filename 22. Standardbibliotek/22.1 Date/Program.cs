e13063cfab7c000f860388d80ab942ff7154cf6e
Emil Jørgensen <emiljorgensen05@gmail.com>
2024-10-10 23:24:07 +0200

    22.1 done

---
+++ 22. Standardbibliotek/22.1 Date/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		// Array med tidsstempler
+		long[] timestamps = { (long)Math.Pow(10, 3), (long)Math.Pow(10, 4), (long)Math.Pow(10, 5), (long)Math.Pow(10, 6) };
+
+		// Loop igennem hvert tidsstempel
+		foreach (long timestamp in timestamps)
+		{
+			// Opret DateTimeOffset objekt fra UnixTimeMilliseconds
+			DateTimeOffset dateTimeOffset = DateTimeOffset.FromUnixTimeMilliseconds(timestamp);
+
+			// Tilgå DateTime attributten
+			DateTime dateTime = dateTimeOffset.DateTime;
+
+			// Udskriv tidspunktet til skærmen
+			Console.WriteLine($"Timestamp: {timestamp}, DateTime: {dateTime.ToString("yyyy - MM - dd HH: mm: ss.fff")}");
+		}
+	}
+}
