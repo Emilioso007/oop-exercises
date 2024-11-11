138e7971b88bd680785509366b67774f805605bf
Emil Jørgensen <emiljorgensen05@gmail.com>
2024-11-11 18:47:11 +0100

    28.1 done

---
+++ 28. Input og Output/28.1 Kommandolinje Argumenter/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Program
+{
+
+	public static void Main(string[] args)
+	{
+
+		if (args.Length == 0)
+		{
+			Console.WriteLine("No arguments provided!");
+		}
+
+		foreach (string s in args)
+		{
+			Console.WriteLine($"Hello {s}");
+		}
+
+		Console.WriteLine("\nPress any key to exit...");
+		Console.ReadKey(true);
+	}
+
+}
