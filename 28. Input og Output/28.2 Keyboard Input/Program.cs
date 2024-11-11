fafb37e03afee6368e1b53d75e2ee1b84b7111b7
Emil Jørgensen <emiljorgensen05@gmail.com>
2024-11-11 18:52:35 +0100

    28.2 done

---
+++ 28. Input og Output/28.2 Keyboard Input/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Program
+{
+
+	public static void Main(string[] args)
+	{
+		Console.WriteLine("Enter a string to reverse: ");
+		string userInput = Console.ReadLine() ?? "";
+
+		string reversedString = string.Join("", userInput.ToCharArray().Reverse());
+
+		Console.WriteLine(reversedString);
+
+		Console.WriteLine("Press any key to exit...");
+		Console.ReadKey(true);
+
+	}
+
+}
