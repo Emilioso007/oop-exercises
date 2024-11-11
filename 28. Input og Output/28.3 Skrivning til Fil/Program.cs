0b1f02dafb84f386b34cdadf6778d7a16b16ca73
Emil Jørgensen <emiljorgensen05@gmail.com>
2024-11-11 19:39:40 +0100

    28.3 and 28.4 done

---
+++ 28. Input og Output/28.3 Skrivning til Fil/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Text.Json;
+
+namespace _28._3_Skrivning_til_Fil;
+
+public class Program
+{
+
+	public static void Main(string[] args)
+	{
+
+		Dictionary<int, string> months = new Dictionary<int, string>
+		{
+			{ 1, "January" },
+			{ 2, "February" },
+			{ 3, "Marts" },
+			{ 4, "April" },
+			{ 5, "May" },
+			{ 6, "June" },
+			{ 7, "July" },
+			{ 8, "August" },
+			{ 9, "September" },
+			{ 10, "October" },
+			{ 11, "November" },
+			{ 12, "December" }
+		};
+
+		try
+		{
+			string json = JsonSerializer.Serialize(months);
+			File.WriteAllText("Months.json", json);
+			Console.WriteLine("Data saved to file: Months.json");
+		}
+		catch (Exception e)
+		{
+			Console.WriteLine("Failed to write! " + e.Message);
+		}
+
+		Console.WriteLine("Press any key to exit...");
+		Console.ReadKey(true);
+
+	}
+
+}
---
+++ 28. Input og Output/28.4 Læsning fra Fil/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json;
+
+namespace _28._4_Læsning_fra_Fil;
+
+public class Program
+{
+
+	public static void Main(string[] args)
+	{
+		Dictionary<int, string> months;
+		
+		string json = File.ReadAllText("Months.json");
+		months = JsonSerializer.Deserialize(json, typeof(Dictionary<int, string>)) as Dictionary<int, string>;
+		
+		foreach (var month in months)
+		{
+			Console.WriteLine($"Key: {month.Key}, Value: {month.Value}");
+		}
+		
+		Console.WriteLine("Press any key to exit...");
+		Console.ReadKey(true);
+	}
+	
+}
