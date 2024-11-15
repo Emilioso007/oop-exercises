40ac1824b873c8a2ca585703f203411a955e4339
Emil Jørgensen <emiljorgensen05@gmail.com>
2024-11-15 13:51:16 +0100

    cleanup from 28.10

--- 28. Input og Output/28.10 Sudoku Visualisering/obj/Debug/net9.0/28.10 Sudoku Visualisering.AssemblyInfo.cs
+++ 28. Input og Output/28.10 Sudoku Visualisering/obj/Debug/net9.0/28.10 Sudoku Visualisering.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("28.10 Sudoku Visualisering")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc8cfd4b854b369c1deb8b9137cd8be6d3d39fd6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3fa320aa4beff47bf7035806d155f34e7d4186f")]
 [assembly: System.Reflection.AssemblyProductAttribute("28.10 Sudoku Visualisering")]
 [assembly: System.Reflection.AssemblyTitleAttribute("28.10 Sudoku Visualisering")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
