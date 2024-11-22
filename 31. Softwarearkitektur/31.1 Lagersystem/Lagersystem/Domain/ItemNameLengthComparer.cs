529825c0f4c721fbf00e513174bfe58247a120ca
Emil Jørgensen <emiljorgensen05@gmail.com>
2024-11-22 12:58:09 +0100

    31. done

---
+++ 31. Softwarearkitektur/31.1 Lagersystem/Lagersystem/Domain/ItemNameLengthComparer.cs
@@ -0,0 +1,12 @@
+
+namespace Domain;
+
+public class ItemNameLengthComparer : IComparer<Item>
+{
+    public int Compare(Item? i1, Item? i2)
+    {
+        if (i1 == null) return 1;
+        if (i2 == null) return -1;
+        return i1.Name.Length - i2.Name.Length;
+    }
+}
---
+++ 31. Softwarearkitektur/31.1 Lagersystem/Lagersystem/Program.cs
@@ -0,0 +1,9 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Presentation.Test.TestMain(args);
+
+        Console.ReadKey();
+    }
+}
